605dffba3a65218f1d78cbaf49088ad974b8fe6a
jayyers <jayyers@gmail.com>
2023-07-07 08:53:43 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Beautiful Jesserka!'));
 app.get('/', (req, res) => res.send('Hello Alyssa!!!!'));
 
 app.listen(port);
